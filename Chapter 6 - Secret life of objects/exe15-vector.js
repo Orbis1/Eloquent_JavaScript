a92a6108ab4afc10cc55eab7a808b7b39fde817f
Кривошеев Дмитрий Владимирович <dvkrivosheev@sberbank.ru>
2021-06-19 00:07:13 +0300

    minor

---
+++ Chapter 6 - Secret life of objects/exe15-vector.js
@@ -0,0 +1,24 @@
+class Vec {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  plus(vector) {
+    const {x, y} = vector;
+    return new Vec(this.x + x, this.y + y)
+  }
+  minus(vector) {
+    const {x, y} = vector;
+    return new Vec(this.x - x, this.y - y)
+  }
+  get length() {
+    return Math.sqrt(this.y**2 + this.x**2);
+  }
+}
+
+console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+// → Vec{x: 3, y: 5}
+console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+// → Vec{x: -1, y: -1}
+console.log(new Vec(3, 4).length);
+// → 5
